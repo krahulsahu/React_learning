847a2707b64f61312017cd4c227b99c3917413e8
Rahul Kumar <rahulkumarsahu8182@gmail.com>
2024-02-09 21:51:48 +0530

    this is my forst repo in react

---
+++ src/components/contacts_page.jsx
@@ -0,0 +1,24 @@
+const contacts = [
+  {
+    name: "Rahul Sahu",
+    imgURL:
+      "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?cs=srgb&dl=pexels-mohamed-abdelghaffar-771742.jpg&fm=jpg",
+    phone: "+91 9876543210",
+    email: "rahulkumarsahu@gmail.com",
+  },
+  {
+    name: "Kishan Kumar",
+      imgURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC8kiSH5ZSAcVoj3tAQQDoP_ux0sSricMyUg&usqp=CAU",
+    phone: "+918765567834",
+    email: "kishankumar@gmail.com",
+  },
+  {
+    name: "Manish  Sharma",
+    imgURL:
+      "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
+    phone: "+918765456782",
+    email: "manishsharma@gmail.com",
+  },
+];
+
+export default contacts;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./components/Card";
+import App from "./components/App"
+
+ReactDOM.render(<App />, document.getElementById("root"));
